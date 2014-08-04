7ad6e2e7f3487a68350345ac2facfd4a8fafe587
Andre Soesilo <asoesilo84@gmail.com>
2014-08-03 20:05:00 -0700

    Fix Angular service path to be absolute

--- app/assets/javascripts/angular/services/projects.js
+++ app/assets/javascripts/angular/services/projects.js
@@ -3,7 +3,7 @@
 var ClockTower = angular.module('ClockTower');
 ClockTower.service('ProjectService', ['$http', 'API_PATH', function($http, apiPath) {
   var getProjects = function() {
-    return $http.get('./api/projects');
+    return $http.get('/api/projects');
   }
 
   return {
--- app/assets/javascripts/angular/services/tasks.js
+++ app/assets/javascripts/angular/services/tasks.js
@@ -3,7 +3,7 @@
 var ClockTower = angular.module('ClockTower');
 ClockTower.service('TaskService', ['$http', 'API_PATH', function($http, apiPath) {
   var getTasks = function() {
-    return $http.get('./api/tasks');
+    return $http.get('/api/tasks');
   }
 
   return {
