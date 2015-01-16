3b7a4e6fe7994c2d02024feaa0ec8b46cc6b117d
Khurram Virani <kvirani@gmail.com>
2015-01-15 18:24:35 -0800

    remove password string column (oopsy)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112050330) do
+ActiveRecord::Schema.define(version: 20150116022340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20150112050330) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
