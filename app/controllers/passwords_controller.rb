1882aa947eabbbfb2028f244c83aefc7838c353d
Graham Grochowski <ggrochow@gmail.com>
2016-01-06 13:56:11 -0800

    password_reset_token to user table

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -9,8 +9,8 @@ class PasswordsController < ApplicationController
   def update
     @user = current_user
     @user.attributes = password_params
-    @user.password_reset_required = false
     if @user.save
+      @user.password_reset_required = false
       redirect_to :root, notice: "Password updated, thanks!"
     else
       render :edit
