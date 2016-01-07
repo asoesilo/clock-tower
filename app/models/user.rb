ce8e7c6ff3e46e21422db7a6276da069a6497321
Graham Grochowski <ggrochow@gmail.com>
2016-01-06 16:49:24 -0800

    uniqueness on password reset token to prevent rare case of random duplicate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, presence: true, uniqueness: true, email: true
+  
+  validates :password_reset_token, uniqueness: true
   validates :password, presence: true, if: :validate_password?
   validates :password_confirmation, presence: true, if: :validate_password?
 
