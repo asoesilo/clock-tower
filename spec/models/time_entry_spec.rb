01c5a267d4cc5b65057b33d63ae0f3811206d502
Graham Grochowski <ggrochow@gmail.com>
2016-02-24 17:15:42 -0800

    add as_json validations to time_entry and user"

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe TimeEntry do
+  before :all do
+    @time_entry = build(:time_entry)
+  end
+
   describe "factory" do
     it "has a valid factory" do
       expect(build(:time_entry)).to be_valid
@@ -30,4 +34,35 @@ describe TimeEntry do
       expect(build(:time_entry, task: nil)).to have(1).errors_on(:task)
     end
   end
+
+  describe "#as_json" do
+    it "should include id" do
+      expect(@time_entry.as_json(root: false)[:id]).to eq(@time_entry.id)
+    end
+
+    it "should include user" do
+      expect(@time_entry.as_json(root: false)[:user]).to_not be_nil
+    end
+
+    it "should include project" do
+      expect(@time_entry.as_json(root: false)[:project]).to_not be_nil
+    end
+
+    it "should include taskb" do
+      expect(@time_entry.as_json(root: false)[:task]).to_not be_nil
+    end
+
+    it "should include date" do
+      expect(@time_entry.as_json(root: false)[:date]).to eq(@time_entry.entry_date)
+    end
+
+    it "should include duration" do
+      expect(@time_entry.as_json(root: false)[:duration_in_hours]).to eq(@time_entry.duration_in_hours)
+    end
+    
+    it "should include comments" do
+      expect(@time_entry.as_json(root: false)[:comments]).to eq(@time_entry.comments)
+    end
+  end
+
 end
