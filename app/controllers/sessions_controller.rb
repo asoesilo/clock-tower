203e5e28dcb60cbe23b9d6d05f0771d6f56cee36
Graham Grochowski <ggrochow@gmail.com>
2016-02-29 15:32:46 -0800

    add case insensitive scope to mimic a find by case insensitive email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to :time_entries
