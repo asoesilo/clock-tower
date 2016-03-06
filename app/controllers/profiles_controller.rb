0509e92c3734be62d07a3e1c12cb4088ca9ac583
Graham Grochowski <ggrochow@gmail.com>
2016-03-06 09:32:28 -0800

    add ability for user to update account information, move user error reporting to shared

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+
+  before_action :load_user
+
+  def edit
+  end
+
+  def update
+    if @user.update(profile_params)
+      redirect_to :root, notice: "Account successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_user
+    @user = current_user
+  end
+
+  def profile_params
+    params.require(:user).permit(:firstname, :lastname, :company_name, :tax_number, :has_tax)
+  end
+
+end
