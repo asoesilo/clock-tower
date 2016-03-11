9e7ed3d8530f8e8792ec25a5fc76c2033b684484
Graham Grochowski <ggrochow@gmail.com>
2016-03-10 17:54:16 -0800

    add date parsing functions to statement period and statement creation interactor

--- app/models/statement_period.rb
+++ app/models/statement_period.rb
@@ -5,4 +5,30 @@ class StatementPeriod < ActiveRecord::Base
   validates :to, inclusion: { in: VALID_TO_FROM }
   validates :draft_days, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
+  def from_date
+    parse_date(from)
+  end
+
+  def to_date
+    parse_date(to)
+  end
+
+  def draft_end_date
+    to_date.advance(days: draft_days)
+  end
+
+  private
+
+  def parse_date(string)
+    today = Date.today
+    case string
+    when 'End of Month'
+      today.end_of_month
+    when 'Start of Month'
+      today.beginning_of_month
+    else
+      Date.new(today.year, today.month, string.to_i)
+    end
+  end
+
 end
