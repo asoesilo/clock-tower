5b266fcb0a71b6bb7c34732e732025010441785d
Graham Grochowski <ggrochow@gmail.com>
2016-03-14 18:46:31 -0700

    move tracker to /new create layout for index page

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,6 +3,15 @@ class TimeEntriesController < ApplicationController
   before_filter :validate_time_entry_update, only: [:edit, :update, :destroy]
 
   def index
+    @all_projects = Project.all
+    @all_tasks = Task.all
+    @from = params[:from]
+    @to = params[:to]
+    @on = params[:on]
+    @time_entries = current_user.time_entries.order(entry_date: :desc)
+  end
+
+  def new
   end
 
 end
