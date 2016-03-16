fc9de9f76ea0c94c0b8c1d59430b3ee10a2e0574
Graham Grochowski <ggrochow@gmail.com>
2016-03-16 10:25:02 -0700

    rand 30 + 1 to prevent 0 from being used

--- spec/factories/statement_periods.rb
+++ spec/factories/statement_periods.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :statement_period do
-    from        { [rand(31), 'End of Month', 'Start of Month'].sample }
-    to          { [rand(31), 'End of Month', 'Start of Month'].sample }
+    from        { [rand(30) + 1, 'End of Month', 'Start of Month'].sample }
+    to          { [rand(30) + 1, 'End of Month', 'Start of Month'].sample }
     draft_days  { rand(31) }
   end
 end
